7b2f26d48f2932c28f0a95732e92a9adf8732c3d
Cat-Div7 <omarashraf11234@gmail.com>
2026-04-03 19:45:00 +0200

    feat: implement basic UI component with useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
+import { useState } from "react";
+
 function App() {
+  const [count] = useState(0);
+
   return (
     <>
       <h1>GitFrame</h1>
+      <OtherComponent count={count} />
     </>
-  )
+  );
 }
+const OtherComponent = ({ count }: { count: number }) => {
+  return <div>{count}</div>;
+};
 
-export default App
+export default App;
